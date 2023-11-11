cf8ce8103244883096c870315f8e10374702681c
tushar0107 <tushardasare1701@gmail.com>
2023-11-11 19:13:49 +0530

    added page

---
+++ index.js
@@ -0,0 +1,14 @@
+const openSearch = ()=>{
+    document.getElementById('search-input').style.top = "4rem";
+    document.getElementById('search-input').style.display = "block";
+}
+const closeSearch = ()=>{
+    document.getElementById('search-input').style.top = "0";
+    document.getElementById('search-input').style.display = "none";
+    document.getElementById('suggestion').style.display = "none";
+    document.getElementById('header-input').value ='';
+}
+const suggestText = ()=>{
+    document.getElementById('suggestion').style.display = "block";
+    console.log(document.getElementById('header-input').value);
+}
